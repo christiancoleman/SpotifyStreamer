9e82591a1fd3d243641bd926df12ea1d81d784da
Christian <ccoleman@acumensolutions.com>
2015-06-01 09:11:08 -0400

    Initial commit

---
+++ app/src/main/java/enfieldacademy/spotifystreamer/SearchResultAdapter.java
@@ -0,0 +1,33 @@
+package enfieldacademy.spotifystreamer;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by para on 5/30/2015.
+ * Used as the adapter that will define how each search result is displayed in a ListView
+ */
+public class SearchResultAdapter extends BaseAdapter{
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+}
